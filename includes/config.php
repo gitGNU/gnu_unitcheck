ccf4cd415678c14aaaa90c1934419e93055443ae
Tom Kaczocha <freedomdeveloper@yahoo.com>
2011-01-10 19:45:47 +1100

    Added WAMP SITE_ROOT define to config.php

--- includes/config.php
+++ includes/config.php
@@ -28,6 +28,9 @@
 
 // SITE_ROOT contains the full path to the SITE_ROOT
 // SITE_ROOT in windows
+// WAMP
+//defined('SITE_ROOT') ? null : define('SITE_ROOT', 'C:' . DS . 'wamp' . DS . 'www' . DS . 'unitcheck');
+// XAMPP
 //defined('SITE_ROOT') ? null : define('SITE_ROOT', 'C:' . DS . 'xampp' . DS . 'htdocs' . DS . 'unitcheck');
 // SITE_ROOT in linux
     defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'opt' . DS . 'lampp' . DS . 'htdocs' . DS . 'unitcheck');
